4c6f349ceebff5f11eeb4fc702cf70a605ec36dc
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-10-16 15:24:17 +0100

    Setting up my CA Code, I will continue to update this code but this is a very rough base of the code at the minute

---
+++ ClientServerSideTCP/src/main/java/com/mycompany/clientserversidetcp/ClientServerSideTCP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.clientserversidetcp;
+
+/**
+ *
+ * @author dmoc2
+ */
+public class ClientServerSideTCP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ ServerSideTCP/src/main/java/com/mycompany/serversidetcp/ServerSideTCP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.serversidetcp;
+
+/**
+ *
+ * @author dmoc2
+ */
+public class ServerSideTCP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
