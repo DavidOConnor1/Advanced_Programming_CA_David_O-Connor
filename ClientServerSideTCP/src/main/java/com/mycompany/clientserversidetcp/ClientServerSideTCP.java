7a4e3bdb42a2911a5685db1578a652d3f884ecfc
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-10-17 16:24:43 +0100

    Finished creating the client server, I will test the two servers together to ensure that they connect and if they do not connect my first assumption will be the port number is not viable

--- ClientServerSideTCP/src/main/java/com/mycompany/clientserversidetcp/ClientServerSideTCP.java
+++ ClientServerSideTCP/src/main/java/com/mycompany/clientserversidetcp/ClientServerSideTCP.java
@@ -8,9 +8,63 @@ package com.mycompany.clientserversidetcp;
  *
  * @author dmoc2
  */
-public class ClientServerSideTCP {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+import java.io.*;
+import java.net.*;
+import java.net.Socket;
+public class ClientServerSideTCP {
+    private static InetAddress host;
+    private static int PORT = 1111;
+
+    public static void main(String[] args) { //start main block
+        try
+        {//start try block
+            host = InetAddress.getLocalHost();
+        }// end try block
+        catch(UnknownHostException e)
+        { //start catch block
+            System.out.println("Host ID is not found");
+            System.exit(1);
+        } //end catch block
+        clientServer();
+    }// end main block
+    
+    private static void clientServer()
+    {//start clientServer
+        Socket link = null; //intializing the link variable
+        try
+        {//start try block
+            link = new Socket(host, PORT); //establishing a host and a port number to connect to as a client 
+            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream())); // Retrives messages from the Server side
+            PrintWriter out = new PrintWriter(link.getOutputStream());
+            
+            BufferedReader userEntry = new BufferedReader(new InputStreamReader(System.in)); //this will be for user entries (currently going to be used for testing to ensure the messages go through until I move over to the event Scheduler)
+            String message = null;
+            String response = null;
+            
+            System.out.println("Enter Message into the Server");
+            message = userEntry.readLine();
+            out.print(message); //sends a stream of the message data to the server
+            response = in.readLine(); //brings the server response to display that it has gone through
+            System.out.println("\n Server Response: "+response);
+            
+        }//end try block
+        catch(IOException e)
+        {//open catch block
+            e.printStackTrace();
+        }//close catch block
+        finally
+        {//open finally block
+           try
+           {//open try block
+            System.out.println("\n closing connection");
+            link.close();
+           }//end try block
+           catch(IOException e)
+           { //open catch block
+                System.out.println("Unable to disconnect/close");
+                System.exit(1);
+           } //end try block
+        }//end finally block
+    }//end clientServer
+}//end client server class
