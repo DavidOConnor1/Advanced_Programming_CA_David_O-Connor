f3c64ee6bf03bb28cca9ad488e8984615f48684d
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-10-17 15:37:52 +0100

    Setting up the server class for the project.

--- ServerSideTCP/src/main/java/com/mycompany/serversidetcp/ServerSideTCP.java
+++ ServerSideTCP/src/main/java/com/mycompany/serversidetcp/ServerSideTCP.java
@@ -8,9 +8,35 @@ package com.mycompany.serversidetcp;
  *
  * @author dmoc2
  */
-public class ServerSideTCP {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+import java.io.*;
+import java.net.*;
+
+public class ServerSideTCP {
+    private static ServerSocket servSock;
+    private static int PORT = 1111;
+    private static int clientConnections =0;
+
+    public static void main(String[] args) {//start main block 
+        
+        System.out.println("Opening Port Connection..."); //displaying server message to check if the class is running 
+        
+        try{
+            
+            servSock = new ServerSocket(PORT); //connecting the server to the port number provided above
+            
+        }
+        catch(IOException e)
+        {
+            System.out.println("Unable to connect to the port");
+        }
+        
+        do
+        {
+            //run class will go here 
+        }
+        while(true);
+        
+        
+    }//end main block
 }
