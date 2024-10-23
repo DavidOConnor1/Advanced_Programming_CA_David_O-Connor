ee84139cf9d42557bd201579e6eafc604f53a24a
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-10-23 16:02:37 +0100

    I have thought of a new way to work on problem we have recieved. My approach would've required me to create a whole extra class orginally but to simplify the means. I decided to ditch that and create new methods within the threads class and work on creating a simple solution through that until I can develop the best coding solution in relation to the consumer and producer homework we gotten 2 weeks ago now

--- ServerSideTCP/src/main/java/com/mycompany/serversidetcp/ClientThreadClass.java
+++ ServerSideTCP/src/main/java/com/mycompany/serversidetcp/ClientThreadClass.java
@@ -14,34 +14,40 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
 
-public class ClientThreadClass implements Runnable {
+public class ClientThreadClass implements Runnable { //start client class
     
     String clientName;
     Socket link = null;
+    PrintWriter out;
+    BufferedReader in;
+    
+    private ArrayList<String> events = new ArrayList<>();
 
     public ClientThreadClass(String clientName, Socket link) {
         this.clientName = clientName;
         this.link = link;
     }
     
-    
+     
+        
     
     
     @Override
     public void run()
     {//start runnable method
-        try{
-            BufferedReader in = new BufferedReader(new InputStreamReader(link.getInputStream())); //reads the values sent from the stream
-            PrintWriter out = new PrintWriter(link.getOutputStream(), true);
-            
-            String command = in.readLine(); //read the command from the user which will be either add, list or remove
-            System.out.println("Command recieved from client" +clientName+" "+command); //this message is to let us check what command has come through the server 
-            
-        } catch(IOException ex)
-        {
-            ex.printStackTrace(); //will print out the errors 
-        } 
+       try{ 
+      
+            in = new BufferedReader(new InputStreamReader(link.getInputStream())); //reads the values sent from the stream
+            out = new PrintWriter(link.getOutputStream(), true); //sends out values 
+            basicOperation(); //loads the normal operation that is required for the multithreading clients
+       } catch(IOException e) {
+           e.printStackTrace();
+       }     
+        
+        
+        /*
         finally{ //start finally block
             
             try
@@ -53,8 +59,63 @@ public class ClientThreadClass implements Runnable {
             catch(IOException ex)
             {//start catch block
                 System.out.println("Unable to Disconnect from Server"); //lets the Server know the that client cannot disconnect
-                System.exit(1); // will exit the entire system if the client cannot discounnect from the server 
+              //  System.exit(1); // will exit the entire system if the client cannot discounnect from the server 
             }//end try block
         }//end finally block
+        */
     } //end runnable method
-}
+    
+    
+    private void basicOperation()
+    {
+                try{
+                    String command;
+            do{
+          
+            
+            //BufferedReader userResponse = new BufferedReader(new InputStreamReader(System.in));
+            
+            command = in.readLine(); //read the command from the user which will be either add, list or remove
+           
+            
+            System.out.println("Command recieved from client" +clientName+" "+command); //this message is to let us check what command has come through the server 
+            
+            if(command.equalsIgnoreCase("add"))
+            {
+                addingOperation();
+            } else if (command.equalsIgnoreCase("remove"))
+            {
+                removingOperation();
+            } 
+            if(command.equalsIgnoreCase("stop"))
+            {
+                System.exit(0);
+            }
+            
+            
+            //out.println("Echo Message: "+ command);
+            } while(true);
+           
+            
+           
+            
+           
+            
+        } catch(IOException ex)
+        {
+            ex.printStackTrace(); //will print out the errors 
+        } 
+    }//end of basic operation
+    
+    
+    private void addingOperation()
+    {
+          
+    }
+    
+    
+    private void removingOperation()
+    {
+        
+    }
+}//end client class 
